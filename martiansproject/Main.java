270a02134786f750151d6b5f3c1d0cd25d5c1304
karina <karina.dyachenko06@gmail.com>
2023-10-21 18:28:24 +0600

    add project to GitHub

---
+++ martiansproject/Main.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+import java.util.Random;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner (System.in);
+		Random random = new Random();
+		
+		int[] cargo = new int [8];
+		int boxKg1 = random.nextInt(710) + 1;
+		int boxKg2 = random.nextInt(713 - boxKg1) + 1;
+		int boxKg3 = 713 - (boxKg1 + boxKg2);
+		int foundCargo = 0;
+		
+		System.out.println("Enter the location (km) ");
+		
+		while (foundCargo != 713) {
+    		int boxKm1 = random.nextInt(8);
+    		int boxKm2 = random.nextInt(8);
+    		int boxKm3 = random.nextInt(8);
+    		cargo[boxKm1] = boxKg1;
+    		cargo[boxKm2] = boxKg2;
+    		cargo[boxKm3] = boxKg3;
+    		System.out.print("Box locations array (hint): ");
+    		for (int i = 0; i < cargo.length; i++) {
+    		    System.out.print(cargo[i] + " ");
+    		}
+    		System.out.print("\n");
+    		int a = scanner.nextInt();
+    		foundCargo += cargo[a];
+    		int b = scanner.nextInt();
+    		foundCargo += cargo[b];
+    		int c = scanner.nextInt();
+    		foundCargo += cargo[c];
+    		if (foundCargo != 713) {
+    		    System.out.println("You did not find the cargo. Enter locations again.");
+    		}
+    		else {
+    		    break;
+    		}
+    		foundCargo = 0;
+    		cargo[boxKm1] = 0;
+    		cargo[boxKm2] = 0;
+    		cargo[boxKm3] = 0;
+		}
+		System.out.print("You found all the cargo.");
+	}
+}
